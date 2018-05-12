d4789e67de1f487e6caf720e8d6b74b9085b04c6
Lomovskoy <lomovskoy.kirill@yandex.ru>
2018-05-12 12:26:49 +0300

    Убран скрипт релактирования текста в статье, он всё ломает

--- build/generated-sources/ap-source-output/entity/Article_.java
+++ build/generated-sources/ap-source-output/entity/Article_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-11T13:26:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-12T11:49:03")
 @StaticMetamodel(Article.class)
 public class Article_ { 
 
